d8e754e4de6385db3e3137f1faaa803a7ed37017
stevenadema <stevenadema@gmail.com>
2020-12-03 01:05:24 -0500

    opening by notation, plotting

--- PGN_DataFrame.py
+++ PGN_DataFrame.py
@@ -1,3 +1,5 @@
+import numpy as np
+
 class PGN_DataFrame:
 	"""PGN class to hold a dataframe object with a record of openings across
 	a date range.
@@ -25,6 +27,7 @@ class PGN_DataFrame:
 		"""	
 		self.df[col] = self.df[move_list].sum(axis=1)
 
+
 	def generate_perc_played(self, col):
 		"""Append a calculated column that displays the percentage of games that 
 		   that have been played with the specified opening.
@@ -33,3 +36,16 @@ class PGN_DataFrame:
 		"""	
 		new_col_name = col + '_%_played'
 		self.df[new_col_name] = self.df[col]/self.df['Total_Games'] * 100
+
+
+	def split_pre_post_release(self, col):
+		"""Split a calculated column into two separate columns so that they can
+		be compared against eachother as separate data points in a matplot chart
+		Args:
+			col: the name of the column to split
+		"""	
+		new_col_name_pre = col + ' pre release'
+		new_col_name_post = col + ' post release'
+		self.df[new_col_name_pre] = self.df[col]
+		self.df[new_col_name_pre] = np.where(self.df['Date'] > '2020-10-23', 0, self.df[new_col_name_pre])
+		self.df[new_col_name_post] = np.where(self.df['Date'] < '2020-10-24', 0, self.df[col])
--- main.py
+++ main.py
@@ -3,35 +3,36 @@ import pandas as pd
 import yaml
 import matplotlib.pyplot as plt
 from PGN_DataFrame import PGN_DataFrame
+from matplotlib.ticker import PercentFormatter
+import matplotlib.dates as mdates
 
 
 def main():
 	"""Maps the popularity of an opening against the Lichess database.
 	Outputs the findings to a matplot line graph.
 	"""
-	# games = r'C:\Users\Steven\Documents\Projects\chess-analysis-master\game.pgn'
-	# games = r'E:\pgns\lichess_db_standard_rated_2020-10.pgn'
+	games = r'C:\Users\Steven\Documents\Projects\chess-analysis-master\data\game.pgn'
+	# games = r'E:\nov_pgns\lichess_db_standard_rated_2020-11.pgn'
 	cfg = get_configs()
 	openings_list = cfg['openings']
-	openings_of_interest = aggregate_similar_moves("Queen's Gambit", openings_list)
+	opening = ['d4','e5','c4']
 	simple_name = 'Queens Gambit'
-	# dates_list = cfg['dates']
-	# # openings_dict = dict()
-	# openings_dict = {ver: {col: 0 for col in openings_list} for ver in dates_list}
+	dates_list = cfg['dates']
+	openings_dict = {ver: {col: 0 for col in openings_list} for ver in dates_list}
 
-	# openings_dict = count_QBs(openings_dict, games)
+	openings_dict = count_QBs(openings_dict, games, opening)
 
-	# df = pd.DataFrame.from_dict(openings_dict,orient='index')
-	# df.to_csv(r'C:\Users\Steven\Documents\Projects\chess-analysis-master\games.csv', sep='|')
-	df = pd.read_csv(r'C:\Users\Steven\Documents\Projects\chess-analysis-master\games.csv', sep='|')
+	df = pd.DataFrame.from_dict(openings_dict, orient='index')
+	# df.to_csv(r'C:\Users\Steven\Documents\Projects\chess-analysis-master\data\games_nov.csv', sep='|') # save for quicker reruns
+	df['Date'] = pd.to_datetime(pd.Series(df['Date']), format="%Y.%m.%d")
 	d = PGN_DataFrame(df)
 	d.generate_total_games()
-	d.sum_similar_moves(simple_name, openings_of_interest)
 	d.generate_perc_played(simple_name)
+	d.split_pre_post_release('Queens Gambit_%_played')
 
 	plot_popularity(d.df, simple_name)
 
-def count_QBs(openings_dict, pgn):
+def count_QBs(openings_dict, pgn, opening):
 	pgn_size = os.path.getsize(pgn)
 	progress = 0
 	with open(pgn, "r") as pgns:
@@ -39,19 +40,45 @@ def count_QBs(openings_dict, pgn):
 				progress = progress + len(line)
 				if line.startswith('[Date'):
 					current_date = line[7:-3:]
-				if line.startswith('[Opening'):
-					opening = line[10:-3:]
+				if line.startswith('1.'):
 					try:
-						openings_dict[current_date][opening] += 1
-					except KeyError:
-						openings_dict[current_date][opening] = 1
-				if progress % 100000 == 0:
+						first_move = line[3:5]
+						second_move = line.split("2. ",1)[1][:2]
+					except:
+						continue
+					if (first_move == opening[0]) and (second_move == opening[2]):
+						openings_dict[current_date]['Queens Gambit'] += 1
+					else:
+						openings_dict[current_date]['Other'] += 1
+				if progress % 1000000 == 0:
 					progressPercent = (100*progress)/pgn_size
 					print(progressPercent)
-				# if progress % 10000000 == 0:
-				# 	break
 
 	return openings_dict
+
+
+def plot_popularity(df, col_name):
+	"""Plot the result
+	"""
+	df = df.set_index('Date')
+	x_list = df.index.values
+	print(x_list)
+	# df = df[['Other','Queens Gambit']]
+	color_map = ['#67B3E0', '#2274A5']
+	print(df['Queens Gambit_%_played pre release'])
+	plt.stackplot(x_list, df['Queens Gambit_%_played pre release'],  df['Queens Gambit_%_played post release'], colors = color_map)
+	plt.legend(loc='upper left')
+	plt.margins(0,0)
+	plt.ylim(8.5, 9.5)
+	plt.axhline(y=9.06, color='#39A3E0', ls='--', xmax=0.38, linewidth=2)
+	plt.axhline(y=9.2, color='#0967A0', ls='--', xmin=0.38, linewidth=2)
+	plt.gca().yaxis.set_major_formatter(PercentFormatter(100, decimals=1))
+	dtFmt = mdates.DateFormatter('%d-%b')
+	plt.gca().xaxis.set_major_formatter(dtFmt)
+	plt.legend(labels=['pre-release avg.', 'post-release avg.'], loc=2)
+	plt.title('The Queen\'s Gambit Opening Popularity')
+	plt.annotate("Release of Netflix series", ('2020-10-24',8.8))
+	plt.show()
 
 
 def get_configs():
@@ -66,24 +93,6 @@ def get_configs():
 	return res
 
 
-def aggregate_similar_moves(begins_with, openings_list):
-	"""Create a list of similar openings by aggregating on openings that 
-	   start with a similar set of characters.
-	Args:
-		begins_with: a string to group opening names
-		openings_list: a list of openings from which to group
-	Returns:
-		most_list: a subset of openings_list containing openings of interest
-	"""	
-	move_list = [i for i in openings_list if i.startswith(begins_with)]
-	return move_list
-
-
-def plot_popularity(df, col_name):
-	ax = plt.gca()
-	y_name = col_name + '_%_played'
-	df.plot(kind='line',x='Date',y=y_name,ax=ax)
-	plt.show()
 
 if __name__ == '__main__':
 	main()
